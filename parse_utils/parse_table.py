1929f23d096aed859b55ec92267d8db7aea23839
odspolianskyi <svobodameni2016@gmail.com>
2025-10-14 17:23:38 +0300

    delete file

--- parse_utils/parse_table.py
+++ parse_utils/parse_table.py
@@ -21,40 +21,6 @@ def parse_first_table(path):
     df = remove_columns(df)
     return rename_columns(df)
 
-
-def parse_upper_second_table(table):
-    """
-    Парсит верхнюю часть таблицы (до "Parameter") как обычную таблицу:
-    Заголовки — первая строка, данные — под ними
-    """
-    table_data = []
-    for row in table.rows:
-        cells = [cell.text.strip() for cell in row.cells]
-        if cells[0].lower().startswith("parameter"):
-            break  # дошли до таблицы анализа — выходим
-        table_data.append(cells)
-
-    # если верхней части таблицы нет возвращаем пустой DataFrame
-    if not table_data:
-        return pd.DataFrame()
-
-    headers = table_data[0]
-    data = table_data[1:]
-
-    # Если строк больше, чем колонок — обрезаем/дополняем
-    df = pd.DataFrame(data, columns=headers)
-    # Удаляем дублирующиеся столбцы
-    df = df.loc[:, ~df.T.duplicated()]
-    # "Повернём" таблицу, чтобы получить: заголовки — первая строка, значения — строки
-    # Т.к. Структура у нас "по строкам", сделаем транспонирование
-    df = df.set_index('Sample (Lot/Batch):').T
-    # переименовываем колонки на корректные
-    df = rename_columns(df)
-
-    df.insert(0, 'Sample (Lot/Batch)', df.index)
-    return df.reset_index(drop=True)
-
-
 def parse_analysis_table(table):
     """
     Парсит таблицу анализа, объединяет Parameter + Method + Unit, удаляет Contract/U,
@@ -98,27 +64,27 @@ def parse_second_table(filepath: str) -> pd.DataFrame:
     df = pd.DataFrame()
     targets = ['Approved by Not approved',
                'Approved by Head of Laboratory',
+               'Checked by Technician laboratory assistant',
                'Weighted average',
                'Checked by Deputy Chief of the Basic Chemistry Department',
-               'Conclusion']
+               'Conclusion',
+               'Name of Pesticide']
 
     for table in doc.tables[1:]:
+        skip_table = False
         for row in table.rows:
             cells = [cell.text.strip() for cell in row.cells]
             if any(x in cells for x in targets):
-                return df
+                skip_table = True
+                break
+        if skip_table:
+            continue  # Переход к следующей таблице
 
-        # Верхняя таблица как нормальный DataFrame
-        upper_df = parse_upper_second_table(table)
-
-        # Нижняя — анализы, как указано
+        # Таблица — анализы
         analysis_df = parse_analysis_table(table)
 
         # Объединяем по горизонтали
-        merged_df = pd.concat([upper_df, analysis_df], axis=1)
-
-        # Объединяем по вертикали
-        df = pd.concat([df, merged_df], ignore_index=True)
+        df = pd.concat([df, analysis_df], axis=1)
 
         df = remove_columns(df)
 
